c3e012c19714a0b17422b2e2851981ae98d77664
daniel camucatto <daniel.camucatto@gmail.com>
2020-10-22 19:58:25 -0300

    adicionando validacao no formulario-home

--- client/src/components/footer/FormContact.js
+++ client/src/components/footer/FormContact.js
@@ -1,19 +1,99 @@
-import React from 'react';
+import React from "react";
+import {useForm} from 'react-hook-form';
 
-export default class FormContact extends React.Component{
-    render(){
-        return(
-            <div className='formContact'>
-                <form name='formContact' id='formContact' method='post' action=''>
-                    <fieldset>
-                        <legend>entre com contato conosco</legend>
-                        <label><input className='name' id='name' type='text' placeholder='Nome'/></label>
-                        <label><input className='email' id='email' type='email' placeholder='Email'/></label>
-                        <label><textarea className='mensagem' id='mensagem' placeholder='Adicione uma mensagem'/></label>
-                        <input className='botton' type='submit' value='Enviar'></input>    
-                    </fieldset>
-                </form>
-            </div>
-        );
-    }
+const initialValue = {
+    name:'',
+    message: '',
 }
+
+const FormContact = () => {
+    const {register, handleSubmit, errors} = useForm();
+    
+    function onSubmit(data){
+        console.log('Data submitted: ', data);
+    }
+    return(
+        <div className='formContact'>
+                <form name='formContact' id='formContact' onSubmit={handleSubmit(onSubmit)} >
+                     <fieldset>
+                         <legend>entre com contato conosco</legend>
+                         <label>
+                             <input 
+                                className='name' 
+                                name='name'
+                                id='name' 
+                                type='text' 
+                                placeholder='Nome'
+                                ref={register({
+                                    required: 'Name',
+                                    pattern:{
+                                        value: /^[A-Z][a-z]* [A-Z][a-z]*/i,
+                                        message: 'Entre com um nome válido'
+                                    }
+                                })}
+                            />
+                            {errors.name && <p className='error'>{errors.name.message}</p>}
+                        </label>
+                         <label>
+                             <input 
+                                className='email' 
+                                name='email'
+                                id='email' 
+                                type='email' 
+                                placeholder='Email'
+                                ref={register({
+                                    required: 'Entre com seu e-mail',
+                                    pattern:{
+                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                        message: "Entre com um e-mail válido",
+                                    }
+                                })}
+                             />
+                             {errors.email && <p className='error'>{errors.email.message}</p>}
+                        </label>
+                         <label>
+                             <textarea 
+                                className='mensagem' 
+                                name='message' 
+                                id='message'
+                                placeholder='Adicione uma mensagem'
+                                ref={register()}
+                                
+                            />
+                            </label>
+                         <input className='botton' type='submit' value='Enviar'></input>    
+                     </fieldset>
+                 </form>
+             </div>
+    )
+}
+
+export default FormContact;
+
+
+
+
+
+
+
+
+
+
+
+// export default class FormContact extends React.Component{
+//     render(){
+//         return(
+//             <div className='formContact'>
+//                 <form name='formContact' id='formContact' method='post' action=''>
+//                     <fieldset>
+//                         <legend>entre com contato conosco</legend>
+//                         <label><input className='name' id='name' type='text' placeholder='Nome'/></label>
+//                         <label><input className='email' id='email' type='email' placeholder='Email'/></label>
+//                         <label><textarea className='mensagem' id='mensagem' placeholder='Adicione uma mensagem'/></label>
+//                         <input className='botton' type='submit' value='Enviar'></input>    
+//                     </fieldset>
+//                 </form>
+//             </div>
+//         );
+//     }
+// }
--- server/api/payment/paymentService.js
+++ server/api/payment/paymentService.js
@@ -3,7 +3,7 @@ const mercadopago = require('mercadopago');
 // Add Your credentials
 const initialize = () => {
     mercadopago.configure({
-        access_token: process.env.MERCADO_PAGO_PROD_ACCESS_TOKEN
+        access_token: 'TEST-2968248292213610-101422-a98050def6e76e13e6e16868d890f9fb-33721125' //process.env.MERCADO_PAGO_PROD_ACCESS_TOKEN
     });
 }
 
